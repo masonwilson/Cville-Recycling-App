24bdadaee19321167aa989d52277186b3cd2c66c
masonwilson <73043051+masonwilson@users.noreply.github.com>
2020-12-28 11:32:24 -0500

    Add files via upload

---
+++ BitmapConfig.cs
@@ -0,0 +1,20 @@
+﻿using App2.Droid;
+using Xamarin.Forms.GoogleMaps;
+using Xamarin.Forms.GoogleMaps.Android.Factories;
+using AndroidBitmapDescriptor = Android.Gms.Maps.Model.BitmapDescriptor;
+using AndroidBitmapDescriptorFactory = Android.Gms.Maps.Model.BitmapDescriptorFactory;
+
+public sealed class BitmapConfig : IBitmapDescriptorFactory
+{
+    public AndroidBitmapDescriptor ToNative(BitmapDescriptor descriptor)
+    {
+        int iconId = 0;
+        switch (descriptor.Id)
+        {
+            case "Recycle":
+                iconId = Resource.Drawable.recycle2;
+                break;
+        }
+        return AndroidBitmapDescriptorFactory.FromResource(iconId);
+    }
+}
